ad08248a3db87d5b6f7f8affaf254fdda88b8379
kei939 <84608396+kei939@users.noreply.github.com>
2021-05-22 13:38:43 +0900

    Initial Commit

---
+++ SwifitUI-Chapter2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwifitUI-Chapter2
+//
+//  Created by 佐藤慶伍 on 2021/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwifitUI-Chapter2/SwifitUI_Chapter2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwifitUI_Chapter2App.swift
+//  SwifitUI-Chapter2
+//
+//  Created by 佐藤慶伍 on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwifitUI_Chapter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
