0031a58994bb9dfcb452de70507bdeb151d4630a
kei939 <84608396+kei939@users.noreply.github.com>
2021-05-22 14:24:25 +0900

    アプリ開発講座(１日目)

--- SwifitUI-Chapter2/ContentView.swift
+++ SwifitUI-Chapter2/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 15.0) {
+            
+            HStack(alignment: .center) {
+            Text("立命館!")
+                .font(.body)
+                .foregroundColor(Color.gray)
+                .padding()
+            Text("よこ")
+                .foregroundColor(Color.purple)
+            }
+            Spacer()
+            Text("iPhone アプリ開発講座")
+                .font(.title)
+                .foregroundColor(Color.blue)
+        }
     }
 }
 
